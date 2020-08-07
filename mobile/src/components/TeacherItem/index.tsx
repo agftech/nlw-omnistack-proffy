63fea67bb25f01bb83e56f287ccb2cf1d15e6135
agftech <agiraof@gmail.com>
2020-08-07 12:15:08 -0300

    #5-day-NLW-2.0 - Add teachers and favorites list

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,10 +1,55 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image, Text } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+
+import heartOutlineIcon from "../../assets/images/icons/heart-outline.png";
+import unFavoriteIcon from "../../assets/images/icons/unfavorite.png";
+import whatsAppIcon from "../../assets/images/icons/whatsapp.png";
 
 import styles from "./styles";
 
 function TeacherItem() {
-	return <View style={styles.container}></View>;
+	return (
+		<View style={styles.container}>
+			<View style={styles.profile}>
+				<Image
+					style={styles.avatar}
+					source={{
+						uri: "https://github.com/agftech.png",
+					}}
+				/>
+				<View style={styles.profileInfo}>
+					<Text style={styles.name}>Agf Tech</Text>
+					<Text style={styles.subject}>Química</Text>
+				</View>
+			</View>
+			<Text style={styles.bio}>
+				Full Stack Developer
+				{"\n"}
+				{"\n"}
+				In constant learning...
+			</Text>
+
+			<View style={styles.footer}>
+				<Text style={styles.price}>
+					Preço/Hora {"   "}
+					<Text style={styles.priceValue}>R$ 20,00</Text>
+				</Text>
+
+				<View style={styles.buttonsContainer}>
+					<RectButton style={[styles.favoriteButton, styles.favorited]}>
+						{/* <Image source={heartOutlineIcon} /> */}
+						<Image source={unFavoriteIcon} />
+					</RectButton>
+
+					<RectButton style={styles.contactButton}>
+						<Image source={whatsAppIcon} />
+						<Text style={styles.contactButtonText}>Entrar em contato</Text>
+					</RectButton>
+				</View>
+			</View>
+		</View>
+	);
 }
 
 export default TeacherItem;
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,15 +1,28 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
 import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
 
 import styles from "./styles";
-import TeacherItem from "../../components/TeacherItem";
 
 function TeacherList() {
 	return (
 		<View style={styles.container}>
 			<PageHeader title='Proffys disponíveis' />
-			<TeacherItem />
+			<ScrollView
+				style={styles.teacherList}
+				contentContainerStyle={{
+					paddingHorizontal: 16,
+					paddingBottom: 16,
+				}}
+			>
+				<TeacherItem />
+				<TeacherItem />
+				<TeacherItem />
+				<TeacherItem />
+				<TeacherItem />
+				<TeacherItem />
+			</ScrollView>
 		</View>
 	);
 }
