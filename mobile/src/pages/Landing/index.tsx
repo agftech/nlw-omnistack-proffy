f58de2db578461f419d8f02513c650cc153b77a9
agftech <agiraof@gmail.com>
2020-08-07 08:16:55 -0300

    #4-day-NLW-2.0 - Update for ReactButton

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { View, Image, Text, TouchableOpacity } from "react-native";
-
+import { View, Image, Text } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
 import styles from "./styles";
 
 import landingImg from "../../assets/images/landing.png";
@@ -19,23 +19,23 @@ function Landing() {
 			</Text>
 
 			<View style={styles.buttonsContainer}>
-				<TouchableOpacity
+				<RectButton
 					onPress={() => {}}
 					style={[styles.button, styles.buttonPrimary]}
 				>
 					<Image source={studyIcon} />
 
 					<Text style={styles.buttonText}>Estudar</Text>
-				</TouchableOpacity>
+				</RectButton>
 
-				<TouchableOpacity
+				<RectButton
 					onPress={() => {}}
 					style={[styles.button, styles.buttonSecondary]}
 				>
 					<Image source={giveClassesIcon} />
 
 					<Text style={styles.buttonText}>Dar aulas</Text>
-				</TouchableOpacity>
+				</RectButton>
 			</View>
 
 			<Text style={styles.totalConnections}>
